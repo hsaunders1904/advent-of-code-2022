04fd01b61758a123d60df423df1d47982bb4222b
Harry Saunders <33317174+hsaunders1904@users.noreply.github.com>
2022-12-06 08:26:10 +0000

    Clean up day06

--- src/aoc22/day06.cpp
+++ src/aoc22/day06.cpp
@@ -1,34 +1,19 @@
 #include "aoc22/aoc.h"
 
+#include <set>
+
 namespace {
-bool has_no_duplicates(const std::array<int, 26> &counts,
-                       const std::string &signal, std::size_t start_idx,
-                       std::size_t window_size) {
-
-  auto it = std::next(signal.begin(), start_idx);
-  return std::all_of(it, std::next(it, window_size),
-                     [&counts](auto ch) { return counts[ch - 'a'] == 1; });
-}
-
 int find_distinct_char_idx(const std::string &signal, std::size_t window_size) {
-  std::array<int, 26> char_counts{0};
-  for (auto i = 0U; i < window_size; ++i) {
-    char_counts[signal[i] - 'a']++;
-  }
-  if (has_no_duplicates(char_counts, signal, window_size, window_size)) {
-    return window_size;
-  }
-  for (auto i = window_size; i < signal.size(); ++i) {
-    char_counts[signal[i - window_size] - 'a']--;
-    char_counts[signal[i] - 'a']++;
-    if (has_no_duplicates(char_counts, signal, i - window_size + 1,
-                          window_size)) {
-      return i + 1;
+  auto window_beg = signal.begin();
+  auto window_end = std::next(window_beg, window_size);
+  for (auto i = 0; i < signal.size() - window_size; ++i) {
+    std::set<char> window(window_beg++, window_end++);
+    if (window.size() == window_size) {
+      return i + window_size;
     }
   }
   return -1;
 }
-
 } // namespace
 
 int day06_1(std::istream *input_file) {
