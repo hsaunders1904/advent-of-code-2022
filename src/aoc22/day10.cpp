41c5d1746d54cd94480c715a2b87236206c70057
Harry Saunders <33317174+hsaunders1904@users.noreply.github.com>
2022-12-10 15:03:16 +0000

    Refactor day10

--- src/aoc22/day10.cpp
+++ src/aoc22/day10.cpp
@@ -2,6 +2,7 @@
 
 namespace {
 constexpr auto ADDX = "addx";
+constexpr auto DISPLAY_WIDTH{40};
 
 std::pair<std::string, int> parse_instruction(const std::string &line) {
   auto parts = split(line, ' ');
@@ -10,57 +11,48 @@ std::pair<std::string, int> parse_instruction(const std::string &line) {
   }
   return {"", 0};
 }
+
+void update_signal(int *signal, int cycle_num, int x_reg) {
+  if ((cycle_num + 20) % DISPLAY_WIDTH == 0) {
+    *signal += x_reg * cycle_num;
+  }
+}
+
+void update_display(std::string *display, int cycle_num, int x_reg) {
+  auto pixel_pos = (cycle_num - 1) % DISPLAY_WIDTH;
+  if (pixel_pos == 0 && cycle_num != 1) {
+    *display += "\n";
+  }
+  if (pixel_pos >= x_reg - 1 && pixel_pos <= x_reg + 1) {
+    *display += "#";
+  } else {
+    *display += ".";
+  }
+}
+
+template <typename T>
+T iter_instructions(std::istream *input_file,
+                    std::function<void(T *, int, int)> accumulator) {
+  int x_reg{1};
+  int cycle_num{1};
+  T acc{};
+  for (std::string line; std::getline(*input_file, line); ++cycle_num) {
+    auto [instruction, value] = parse_instruction(line);
+    accumulator(&acc, cycle_num, x_reg);
+    if (instruction == ADDX) {
+      cycle_num++;
+      accumulator(&acc, cycle_num, x_reg);
+      x_reg += value;
+    }
+  }
+  return acc;
+}
 } // namespace
 
 int day10_1(std::istream *input_file) {
-  int x_reg{1};
-  std::size_t cycle_num{1};
-  int signal_strength_sum{0};
-  for (std::string line; std::getline(*input_file, line); ++cycle_num) {
-    auto [instruction, value] = parse_instruction(line);
-    if ((cycle_num + 20) % 40 == 0) {
-      signal_strength_sum += x_reg * cycle_num;
-    }
-    if (instruction == ADDX) {
-      cycle_num++;
-      if ((cycle_num + 20) % 40 == 0) {
-        signal_strength_sum += x_reg * cycle_num;
-      }
-      x_reg += value;
-    }
-  }
-  return signal_strength_sum;
+  return iter_instructions<int>(input_file, update_signal);
 }
 
 std::string day10_2(std::istream *input_file) {
-  int x_reg{1};
-  int cycle_num{1};
-  std::string display;
-  for (std::string line; std::getline(*input_file, line); ++cycle_num) {
-    auto [instruction, value] = parse_instruction(line);
-    auto pixel_pos = (cycle_num - 1) % 40;
-    if (pixel_pos == 0 && cycle_num != 1) {
-      display += "\n";
-    }
-    if (pixel_pos >= x_reg - 1 && pixel_pos <= x_reg + 1) {
-      display += "#";
-    } else {
-      display += ".";
-    }
-    if (instruction == ADDX) {
-      cycle_num++;
-      pixel_pos = (cycle_num - 1) % 40;
-      if (pixel_pos == 0) {
-        display += "\n";
-      }
-      if (pixel_pos >= x_reg - 1 && pixel_pos <= x_reg + 1) {
-        display += "#";
-      } else {
-        display += ".";
-      }
-      x_reg += value;
-    }
-  }
-  display += "\n";
-  return display;
+  return iter_instructions<std::string>(input_file, update_display) + "\n";
 }
