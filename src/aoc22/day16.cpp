3b75a387c2a8115e31b3dd534948da4b58937eab
Harry Saunders <33317174+hsaunders1904@users.noreply.github.com>
2023-09-12 19:45:37 +0100

    Remove 'to_array' from State in day16

--- src/aoc22/day16.cpp
+++ src/aoc22/day16.cpp
@@ -18,8 +18,8 @@ read_valves(std::istream *input) {
     auto valve_name = parts[1];
     auto flow_rate_str = split(parts[4], '=')[1];
     flow_rate_str.pop_back(); // remove semicolon
-    std::string leads_to_str = std::accumulate(std::next(parts.begin(), 9),
-                                               parts.end(), std::string());
+    std::string leads_to_str =
+        std::accumulate(std::next(parts.begin(), 9), parts.end(), std::string());
     flow_rates[valve_name] = std::stoul(flow_rate_str);
     leads_to[valve_name] = split(leads_to_str, ',');
   }
@@ -44,18 +44,6 @@ read_valves(std::istream *input) {
   return {flow_rate, adj_list};
 }
 
-// https://stackoverflow.com/questions/42701688/using-an-unordered-map-with-arrays-as-keys
-template <std::size_t N> struct ArrayHasher {
-  std::size_t operator()(const std::array<std::size_t, N> &arr) const {
-    std::size_t h{0};
-    for (auto e : arr) {
-      h ^= std::hash<int>{}(e) + 0x9e3779b9 + (h << 6) + (h >> 2);
-    }
-    return h;
-  }
-};
-using StateSet = std::unordered_set<std::array<std::size_t, 4>, ArrayHasher<4>>;
-
 Vec2d<std::size_t>
 floyd_warshall(const std::vector<std::vector<std::size_t>> &adj_list) {
   Vec2d<std::size_t> dist(adj_list.size(), adj_list.size(),
@@ -69,8 +57,7 @@ floyd_warshall(const std::vector<std::vector<std::size_t>> &adj_list) {
   for (auto k = 0U; k < adj_list.size(); ++k) {
     for (auto i = 0U; i < adj_list.size(); ++i) {
       for (auto j = 0U; j < adj_list.size(); ++j) {
-        dist.get(i, j) =
-            std::min(dist.get(i, j), dist.get(i, k) + dist.get(k, j));
+        dist.get(i, j) = std::min(dist.get(i, j), dist.get(i, k) + dist.get(k, j));
       }
     }
   }
@@ -82,13 +69,27 @@ struct State {
   std::size_t position;
   std::size_t time_spent;
   std::size_t pressure_relieved;
-  std::array<std::size_t, 4> to_array() const {
-    return {opened.to_ulong(), position, time_spent, pressure_relieved};
+  bool operator==(const State &other) const {
+    return (opened == other.opened) && (position == other.position) &&
+           (time_spent == other.time_spent) &&
+           (pressure_relieved == other.pressure_relieved);
   }
 };
 
-std::size_t sum_flow_rate(const State &state,
-                          const std::vector<std::size_t> flow_rate) {
+// https://stackoverflow.com/questions/42701688/using-an-unordered-map-with-arrays-as-keys
+struct StateHasher {
+  std::size_t operator()(const State &state) const {
+    std::size_t h{0};
+    for (auto e : {state.opened.to_ulong(), state.position, state.time_spent,
+                   state.pressure_relieved}) {
+      h ^= std::hash<int>{}(e) + 0x9e3779b9 + (h << 6) + (h >> 2);
+    }
+    return h;
+  }
+};
+using StateSet = std::unordered_set<State, StateHasher>;
+
+std::size_t sum_flow_rate(const State &state, const std::vector<std::size_t> flow_rate) {
   std::size_t flow_rate_sum{0};
   for (auto i = 0U; i < flow_rate.size(); ++i) {
     if (state.opened[i]) {
@@ -98,17 +99,15 @@ std::size_t sum_flow_rate(const State &state,
   return flow_rate_sum;
 }
 
-std::size_t sim_to_end(const State &state,
-                       const std::vector<std::size_t> &flow_rate,
+std::size_t sim_to_end(const State &state, const std::vector<std::size_t> &flow_rate,
                        const std::size_t time_budget) {
   auto relieved_per_min = sum_flow_rate(state, flow_rate);
   auto time_remaining = time_budget - state.time_spent;
   return state.pressure_relieved + relieved_per_min * time_remaining;
 }
 
-std::vector<std::size_t>
-unopened_flowing(const std::vector<std::size_t> &flow_rate,
-                 const ul_bitset opened) {
+std::vector<std::size_t> unopened_flowing(const std::vector<std::size_t> &flow_rate,
+                                          const ul_bitset opened) {
   std::vector<std::size_t> uof;
   for (auto i = 0U; i < flow_rate.size(); ++i) {
     if (flow_rate[i] > 0 && !opened[i]) {
@@ -118,8 +117,7 @@ unopened_flowing(const std::vector<std::size_t> &flow_rate,
   return uof;
 }
 
-bool all_valves_open(const std::vector<std::size_t> &flow_rate,
-                     const ul_bitset opened) {
+bool all_valves_open(const std::vector<std::size_t> &flow_rate, const ul_bitset opened) {
   for (auto i = 0U; i < flow_rate.size(); ++i) {
     if (flow_rate[i] > 0 && !opened[i]) {
       return false;
@@ -147,7 +145,7 @@ int day16_1(std::istream *input_file) {
   State initial_state({0, 0, 0, 0});
   std::queue<State> states;
   states.push(initial_state);
-  StateSet visited_states{initial_state.to_array()};
+  StateSet visited_states{initial_state};
   std::size_t max_pressure_relieved{0};
   while (states.size() > 0) {
     auto state = states.front();
@@ -168,11 +166,11 @@ int day16_1(std::istream *input_file) {
         continue;
       }
       // Build the new state of things
-      const auto new_state = update_state(
-          state, true, dest, sum_flow_rate(state, flow_rate) * cost, cost);
+      const auto new_state =
+          update_state(state, true, dest, sum_flow_rate(state, flow_rate) * cost, cost);
 
-      if (visited_states.find(new_state.to_array()) == visited_states.end()) {
-        visited_states.insert(state.to_array());
+      if (visited_states.find(new_state) == visited_states.end()) {
+        visited_states.insert(state);
         states.push(new_state);
       }
     }
